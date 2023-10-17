b69c0824616f9187195e178fbfcd480969e0a685
kennyatu23 <G00267173@atu.ie>
2023-10-17 18:39:31 +0100

    commitRegistration1

--- src/main/java/ie/atu/registrationservice/RegistrationServiceApplication.java
+++ src/main/java/ie/atu/registrationservice/RegistrationServiceApplication.java
@@ -2,11 +2,15 @@ package ie.atu.registrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class RegistrationServiceApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         SpringApplication.run(RegistrationServiceApplication.class, args);
     }
 
